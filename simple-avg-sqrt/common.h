0175cb75b6fda034438542785ade80c89553a08d
Saeid Aliei <saeidaliei@gmail.com>
2023-10-01 14:48:34 +0200

    add MPI and CUDA error check macros

--- simple-avg-sqrt/common.h
+++ simple-avg-sqrt/common.h
@@ -2,6 +2,7 @@
 #define COMMON_H 1
 
 #include <mpi.h>
+#include <cuda.h>
 
 #define MPIErrorCheck(call) \
     do { \
@@ -10,8 +11,21 @@
             char err_str[MPI_MAX_ERROR_STRING]; \
             int err_str_len = 0; \
             MPI_Error_string(__ierr, err_str, &err_str_len); \
-            fprintf(stderr, "MPI Error: %s:%d %d:%s\n", __FILE__, __LINE__, __ierr, err_str); \
+            if(rank == 0) { \
+                fprintf(stderr, "MPI Error: %s:%d %d:%s\n", __FILE__, __LINE__, __ierr, err_str); \
+            }
             MPI_Abort(MPI_COMM_WORLD, 1); \
+        } \
+    } while(0)
+
+#define CUDAErrorCheck(call) \
+    do { \
+        cudaError_t cuErr = (call); \
+        if(cuErr != cudaSuccess) { \
+            if(rank == 0) { \
+                fprintf(stderr, "CUDA Error: %s:%d %d:%s\n", __FILE__, __LINE__, cuErr, cudaGetErrorString(cuErr)); \
+            } \
+            MPI_Abort(MPI_COMM_WORLD, 2); \
         } \
     } while(0)
 
